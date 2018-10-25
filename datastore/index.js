448a3ccb385f64dc717fd1bb8eabd955b7f32965
Monkey Do <monkeydo@Monkeys-MacBook-Pro.local>
2018-10-24 19:48:29 -0700

    Bare Miniumum Requirements Completed

--- datastore/index.js
+++ datastore/index.js
@@ -28,13 +28,37 @@ exports.create = (text, callback) => {
 
 exports.readAll = (callback) => {
   var data = [];
+  let id;
   fs.readdir(`${exports.dataDir}`, (err, files) => {
+    // console.log('files: ', files);
+    // map through the files
+    // grab the id (extract) - then do an async readFile
+    // in a then block return the text
+    // in the next then return the promise.all
+
     files.forEach(file => {
-      let id = file.slice(0, -4);
-      let text = id;
-      data.push({ id, text });
+      id = file.slice(0, -4);
+      data.push(id);
+      //console.log('data from in the forEach okay? ---> ', data);
     });
-    callback(null, data);
+    var promises = data.map(id => {
+      //console.log('file --> ', id);
+      return new Promise((resolve, reject) => {
+        //var filePath = path.join(`${exports.dataDir}/${id}.txt`);
+        fs.readFile(path.join(`${exports.dataDir}/${id}.txt`), 'utf8', (err, text) => {
+          if (err) {
+            return reject(err);
+          }
+          //console.log('*******------->>>', id, text);
+          resolve({id, text});
+        });
+      });
+    });
+    Promise.all(promises).then(function(values) {
+      //console.log(' our fine values --------->', values);
+      //solution.push({ id, text });
+      callback(null, values);
+    });
   });
 };
 
@@ -81,7 +105,6 @@ exports.delete = (id, callback) => {
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
-
 exports.dataDir = path.join(__dirname, 'data');
 
 exports.initialize = () => {
